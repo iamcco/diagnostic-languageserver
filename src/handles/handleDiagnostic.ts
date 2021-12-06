iamcco/diagnostic-languageserver

commit da822a15a2ad7bacfb6b574228c5decd93cf3584

Allow using %filepath placeholder in errorsRoot (#77)
* Allow using %filepath placeholder in errorsRoot

* Update README

src/handles/handleDiagnostic.ts
@@ -109,14 +109,13 @@ function formatStringWithObject<T extends Record<string, any> | any[]>(
   });
 }
 
-function handleLinterJson(currentSourceName: string, output: string, config: ILinterConfig): ILinterResult[] {
+function handleLinterJson(currentSourceName: string, output: string, config: ILinterConfig, fpath: string): ILinterResult[] {
   if (!config.parseJson) {
     throw new Error('missing parseJson')
   }
 
   const {
     sourceName,
-    errorsRoot,
     line,
     column,
     endLine,
@@ -125,8 +124,12 @@ function handleLinterJson(currentSourceName: string, output: string, config: ILi
     message,
   } = config.parseJson
 
+  const errorsRoot = (typeof config.parseJson.errorsRoot === 'string')
+    ? config.parseJson.errorsRoot.replace('%filepath', fpath)
+    : config.parseJson.errorsRoot.map(v => v.replace('%filepath', fpath));
+
   const resultsFromJson: any[] = errorsRoot
-    ? lodashGet(JSON.parse(output), errorsRoot, [])
+  ? lodashGet(JSON.parse(output), errorsRoot, [])
     : JSON.parse(output)
 
   return resultsFromJson.map<ILinterResult>(jsonObject => {
@@ -233,7 +236,7 @@ async function handleLinter (
     }
 
     let linterResults: ILinterResult[] = config.parseJson
-      ? handleLinterJson(sourceName, output, config)
+    ? handleLinterJson(sourceName, output, config, URI.parse(textDocument.uri).fsPath)
       : handleLinterRegex(sourceName, output, config)
 
     // Check if we should filter based on the sourceName.

commit c2523a610c769b66c8ed8f78e63eda84f304c7e2

fix(diagnosis): allow config.errorsRoot to be empty
Resolves a regression introduced in #77. The case where
config.errorsRoot is empty is not handled properly, resulting in
diagnosis failing when it is missing.

Only attempt to replace the %filepath placeholder after verifying
config.errorsRoot is non-empty.

src/handles/handleDiagnostic.ts
@@ -109,6 +109,12 @@ function formatStringWithObject<T extends Record<string, any> | any[]>(
   });
 }
 
+function replaceFilepath(errorsRoot: string | string[], fpath: string) {
+  return (typeof errorsRoot === 'string')
+    ? errorsRoot.replace('%filepath', fpath)
+    : errorsRoot.map(v => v.replace('%filepath', fpath))
+}
+
 function handleLinterJson(currentSourceName: string, output: string, config: ILinterConfig, fpath: string): ILinterResult[] {
   if (!config.parseJson) {
     throw new Error('missing parseJson')
@@ -116,6 +122,7 @@ function handleLinterJson(currentSourceName: string, output: string, config: ILi
 
   const {
     sourceName,
+    errorsRoot,
     line,
     column,
     endLine,
@@ -124,12 +131,8 @@ function handleLinterJson(currentSourceName: string, output: string, config: ILi
     message,
   } = config.parseJson
 
-  const errorsRoot = (typeof config.parseJson.errorsRoot === 'string')
-    ? config.parseJson.errorsRoot.replace('%filepath', fpath)
-    : config.parseJson.errorsRoot.map(v => v.replace('%filepath', fpath));
-
   const resultsFromJson: any[] = errorsRoot
-  ? lodashGet(JSON.parse(output), errorsRoot, [])
+    ? lodashGet(JSON.parse(output), replaceFilepath(errorsRoot, fpath), [])
     : JSON.parse(output)
 
   return resultsFromJson.filter(jsonObject => {
@@ -238,7 +241,7 @@ async function handleLinter (
     }
 
     let linterResults: ILinterResult[] = config.parseJson
-    ? handleLinterJson(sourceName, output, config, URI.parse(textDocument.uri).fsPath)
+      ? handleLinterJson(sourceName, output, config, URI.parse(textDocument.uri).fsPath)
       : handleLinterRegex(sourceName, output, config)
 
     // Check if we should filter based on the sourceName.